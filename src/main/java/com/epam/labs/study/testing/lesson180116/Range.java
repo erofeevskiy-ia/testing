73756dce38b1260460ac3b712890e57cd5c6d4d3
Igor <igorer27@yandex.ru>
2018-01-21 16:51:16 +0300

    Implements Range

--- src/main/java/com/epam/labs/study/testing/lesson180116/Range.java
+++ src/main/java/com/epam/labs/study/testing/lesson180116/Range.java
@@ -1,18 +1,43 @@
 package com.epam.labs.study.testing.lesson180116;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Range implements RangeInterface {
+
+    private long lowBound;
+    private long upperBound;
+
+    public Range(long lowBound, long upperBound) {
+        if (lowBound > upperBound) throw new IllegalArgumentException("Mistake: lowBound > upperBound");
+        this.lowBound = lowBound;
+        this.upperBound = upperBound;
+    }
+
+    private class Itr implements Iterator<Long> {
+        long cursor = getLowerBound();
+
+        @Override
+        public boolean hasNext() {
+            return cursor<=getUpperBound();
+        }
+
+        @Override
+        public Long next() {
+            return cursor++;
+        }
+    }
+
     @Override
     public boolean isBefore(Range otherRange) {
 
-        return false;
+        return this.upperBound < otherRange.getLowerBound();
     }
 
     @Override
     public boolean isAfter(Range otherRange) {
-        return false;
+        return this.lowBound > otherRange.getUpperBound();
     }
 
     @Override
@@ -22,26 +47,30 @@ public class Range implements RangeInterface {
 
     @Override
     public long getLowerBound() {
-        return 0;
+        return this.lowBound;
     }
 
     @Override
     public long getUpperBound() {
-        return 0;
+        return this.upperBound;
     }
 
     @Override
     public boolean contains(long value) {
-        return false;
+        return (value >= this.getLowerBound() && value<=this.getUpperBound());
     }
 
     @Override
     public List<Long> asList() {
-        return null;
+        List<Long> list = new ArrayList<>();
+        Iterator<Long> it = asIterator();
+        while(it.hasNext())
+            list.add(it.next());
+        return list;
     }
 
     @Override
     public Iterator<Long> asIterator() {
-        return null;
+        return new Itr();
     }
 }
